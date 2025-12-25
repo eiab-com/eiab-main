52175b02c63d9fda9e713ecb97bbb01cd9022733
eiab <exploreindiaandbeyond@gmail.com>
2025-12-25 17:48:07 +0530

    Revert "migration"
    This reverts commit 95ea06c4ee01c0f9995691da090c1f2512b7f897.

--- src/components/adventures/info-item.tsx
+++
@@ -1,8 +0,0 @@
-const InfoItem = ({ title, value }: { title: string; value: string }) => (
-  <div className=" flex flex-col gap-1 sm:gap-2 lg:flex-row lg:justify-between">
-    <span className="text-muted-foreground">{title}</span>
-    <span className="font-medium">{value}</span>
-  </div>
-);
-
-export default InfoItem;
--- src/components/ui/contact-button/contact-button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "../button";
-
-const ContactButton = () => {
-  return <Button  className="rounded-full">Get in Touch</Button>;
-};
-
-export default ContactButton;
